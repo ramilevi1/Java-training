c050012a5fcd92986ba2753f277d3cbd552950d1
ramilevi1 <60967282+ramilevi1@users.noreply.github.com>
2020-02-17 17:12:10 +0700

    Update helloWorld.java

--- javaTraining/src/basics/helloWorld.java
+++ javaTraining/src/basics/helloWorld.java
@@ -4,5 +4,6 @@ public class helloWorld {
 	public static void main(String[] args) {
 		System.out.println("hellow world!");
 		System.out.println("i am adding code to my project");
+		System.out.println("adding code from github);
 	}
 }
