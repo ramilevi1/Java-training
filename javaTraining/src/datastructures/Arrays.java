e449fca3fa4cf413a72d38c906588968732a7148
Rami <levi.rami@gmail.com>
2020-02-21 12:32:38 +0700

    adding new file lab3A

---
+++ javaTraining/src/datastructures/Arrays.java
@@ -0,0 +1,14 @@
+package datastructures;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		String [] alphabet = {"a","b","c","d","e","f"};
+		
+		for (String i : alphabet) {
+			System.out.println(i);
+		}
+
+	}
+
+}
